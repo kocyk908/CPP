c642c0cc5e6d67743cb43f9c3cb5db09acc8be5e
Lukasz Koc <lkoc@c3r14s2.42warsaw.pl>
2024-10-07 18:50:18 +0200

    added

--- CPP01/main.cpp
+++ CPP01/main.cpp
@@ -1,9 +1,20 @@
 #include <iostream>
 
-void add_contact();
+class Contact
+{
+public:
+	std::string first_name;
+	std::string last_name;
+	std::string phone_number;
+	std::string secret;
+};
+
+void add_contact(Contact contacts[], int i);
 
 int main()
 {
+	int i = 1;
+	Contact contacts[8]; 
 	std::string command;
 	std::cout << "Welcome to the PhoneBook!\n";
 	std::cout << "Please print option from listed below:\n";
@@ -11,7 +22,9 @@ int main()
 	std::getline(std::cin, command);
 	if (command == "ADD")
 	{
-		add_contact();
+		add_contact(contacts, i);
+		std::cout << contacts[i].first_name;
+		i++;
 	}
 	else if (command == "SEARCH")
 	{
@@ -28,23 +41,48 @@ int main()
 	}
 }
 
-
-void add_contact()
+std::string add_input(std::string str)
 {
+	int i = 0;
+	std::getline(std::cin, str);
+	while (str.empty())
+	{
+		if (i == 0)
+		{
+			std::cout << "You can't leave blank field. Please try again: \n";
+			i++;
+		}
+		std::getline(std::cin, str);
+	}
+	return (str);
+
+}
+
+void add_contact(Contact contacts[], int i)
+{
+	
+	if (i == 8)
+	{
+		i = 1;
+	}
 	std::string first_name;
 	std::string last_name;
 	std::string phone_number;
 	std::string secret;
 	std::cout << "Please enter first name: ";
-	std::getline(std::cin, first_name);
+	first_name = add_input(first_name);
 	std::cout << "Please enter last name: ";
-	std::getline(std::cin, last_name);
+	last_name = add_input(last_name);
 	std::cout << "Please enter phone number: ";
-	std::getline(std::cin, phone_number);
+	phone_number = add_input(phone_number);
 	std::cout << "Please enter secret: ";
-	std::getline(std::cin, secret);
+	secret = add_input(secret);
 
-	std::cout << "Contact added successfully!\n"; 
+	contacts[i].first_name = first_name;
+	contacts[i].last_name = last_name;
+	contacts[i].phone_number = phone_number;
+	contacts[i].secret = secret;
+	std::cout << "Contact added successfully!\n";
 }
 //void search_contact();
 //void exit();
